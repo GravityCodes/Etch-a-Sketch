7692f74ba5f6427208df6a5f9fa8fbd7609d4725
Johan Mesa <Johanotr@gmail.com>
2024-08-03 11:35:44 -0400

    Added slider stlye

--- script/app.js
+++ script/app.js
@@ -3,6 +3,7 @@ const container = document.querySelector(".container");
 const gridBlockAmountSubmit = document.querySelector("#grid-block-amount-submit");
 const gridBlockAmount = document.querySelector("#grid-block-amount");
 let allGridBlocks = document.querySelectorAll(".gridBlock");
+const reset = document.querySelector("#reset");
 
 let createGrid = (num) => {
     amount = num ** 2
@@ -17,7 +18,7 @@ let createGrid = (num) => {
 
     for(let i=0; i < amount; i++) {
         const gridBlock = document.createElement("div");
-        gridBlock.style = `aspect-ratio: 1/1; width: ${800 / num}px ; border: .5px solid black; box-sizing: border-box; user-select: none; `;
+        gridBlock.style = `aspect-ratio: 1/1; width: ${800 / num}px ; border: .1px solid #184E77; border-radius: 5px; box-sizing: border-box; user-select: none; `;
         gridBlock.setAttribute('class', 'gridBlock');
         container.appendChild(gridBlock);
     }
@@ -47,8 +48,14 @@ let createGrid = (num) => {
 // Create Grid
 createGrid(16)
 
+// Keeps track of the current grid being used for resetting purposes.
+let currentGridAmount;
 
+gridBlockAmountSubmit.addEventListener("click", () => {
+    createGrid(gridBlockAmount.value);
+    return currentGridAmount = gridBlockAmount.value;
+});
 
-
-
-gridBlockAmountSubmit.addEventListener("click", () => createGrid(gridBlockAmount.value))
+reset.addEventListener("click", () => {
+    createGrid(currentGridAmount);
+})
